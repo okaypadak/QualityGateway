765b8466a75e375b4b2ae88d77194b9ee4eaf751
Okay Padak <kypdk@proton.me>
2025-05-14 01:29:33 +0300

    güncel

--- src/main/java/dev/padak/backend/QualityGateway/JwtAuthFilter.java
+++ src/main/java/dev/padak/backend/QualityGateway/JwtAuthFilter.java
@@ -1,13 +1,21 @@
 package dev.padak.backend.QualityGateway;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
-import org.springframework.cloud.gateway.filter.factory.GatewayFilterFactory;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
 @Component
-public class JwtAuthFilter implements GatewayFilterFactory<JwtAuthFilter.Config> {
+public class JwtAuthFilter extends AbstractGatewayFilterFactory<JwtAuthFilter.Config> {
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    public JwtAuthFilter() {
+        super(Config.class);
+    }
 
     @Override
     public GatewayFilter apply(Config config) {
@@ -18,12 +26,19 @@ public class JwtAuthFilter implements GatewayFilterFactory<JwtAuthFilter.Config>
                 return exchange.getResponse().setComplete();
             }
 
-            // sadece header kontrolü, body yok!
+            String token = authHeader.substring(7);
+
+            try {
+                jwtUtil.validateToken(token);
+            } catch (Exception e) {
+                exchange.getResponse().setStatusCode(HttpStatus.FORBIDDEN);
+                return exchange.getResponse().setComplete();
+            }
+
             return chain.filter(exchange);
         };
     }
 
-    public static class Config {
-    }
+    public static class Config {}
 }
 
